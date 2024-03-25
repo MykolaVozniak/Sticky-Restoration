26a40c894c97d2c95098d52dc763a179b9618252
MVozniak <mikola.v.2016@gmail.com>
2024-03-25 11:49:59 +0200

    Added state machine (pause)

--- src/scripts/Figure.gd
+++ src/scripts/Figure.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
-const TILE_SIZE: int = 8 #qq
+@onready var game_manager = %GameManager
+var tile_size: int
+
 const SPEED_NORMAL: float = 300
 const SPEED_SLOW: float = 70
 var speed: float = SPEED_NORMAL
@@ -19,34 +21,45 @@ var is_direction_process = false
 var next_tile
 
 func _ready():
-	pass
+	tile_size = game_manager.TILE_SIZE
+	current_direction = _get_random_direction()
 
-func _physics_process(delta):
-#To avoid the "rollback" effect when changing direction, 
-#we change direction not relative to our current position, 
-#but relative to the position we were heading for.
-	if is_direction_changed:
-		next_tile = next_tile.snapped(Vector2(TILE_SIZE, TILE_SIZE)) + current_direction * TILE_SIZE
-		is_direction_changed = false
-		is_direction_process = true
-	elif is_direction_process:
-		next_tile = next_tile
-		if(position.snapped(Vector2(TILE_SIZE, TILE_SIZE)) == next_tile):
-			is_direction_process = false
-	else:
-		next_tile = position.snapped(Vector2(TILE_SIZE, TILE_SIZE)) + current_direction * TILE_SIZE
-	
-	velocity = (next_tile - position).normalized() * speed
-	move_and_slide()
+func _physics_process(_delta):
+	if game_manager.current_state == game_manager.GameState.RUNNING:
+	#To avoid the "rollback" effect when changing direction, 
+	#we change direction not relative to our current position, 
+	#but relative to the position we were heading for.
+		if is_direction_changed:
+			next_tile = next_tile.snapped(Vector2(tile_size, tile_size)) + current_direction * tile_size
+			is_direction_changed = false
+			is_direction_process = true
+		elif is_direction_process:
+			next_tile = next_tile
+			if(position.snapped(Vector2(tile_size, tile_size)) == next_tile):
+				is_direction_process = false
+		else:
+			next_tile = position.snapped(Vector2(tile_size, tile_size)) + current_direction * tile_size
+		
+		velocity = (next_tile - position).normalized() * speed
+		move_and_slide()
 
 func _input(_event):
-	for dir in directions.keys():
-		if Input.is_action_just_pressed(dir):
-			current_direction = directions[dir]
-			is_direction_changed = true
+	if game_manager.current_state == game_manager.GameState.RUNNING:
+		for dir in directions.keys():
+			if Input.is_action_just_pressed(dir):
+				current_direction = directions[dir]
+				is_direction_changed = true
 			
 func speed_to_normal():
 	speed = SPEED_NORMAL
 	
 func speed_to_slow():
 	speed = SPEED_SLOW
+	
+func _get_random_direction():
+	var direction_keys = directions.keys()
+	var random_index = randi() % direction_keys.size()
+	var random_direction_key = direction_keys[random_index]
+	var random_direction = directions[random_direction_key]
+	return random_direction
+
